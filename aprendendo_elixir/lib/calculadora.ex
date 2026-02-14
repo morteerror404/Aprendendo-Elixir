5380272c5c6c927ca89db81efeea40b188e43869
morteerror404 <jfrota@stacksec.com.br>
2026-02-14 18:45:21 -0300

    Atualizando documentação

--- aprendendo_elixir/lib/calculadora.ex
+++ aprendendo_elixir/lib/calculadora.ex
@@ -4,16 +4,5 @@ defmodule Calculadora do
   """
 
   # Aqui o Elixir escolhe a função baseada no átomo que passamos (:soma, :sub, etc)
-  def calcular(:soma, a, b), do: a + b
 
-  def calcular(:subtracao, a, b), do: a - b
-
-  def calcular(:multiplicacao, a, b), do: a * b
-
-  # Tratamento especial para divisão por zero usando Pattern Matching direto no argumento
-  def calcular(:divisao, _a, 0), do: {:error, "Não dividiras por zero, amém!"}
-  def calcular(:divisao, a, b), do: {:ok, a / b}
-
-  # Caso o usuário envie algo que não mapeamos
-  def calcular(_, _, _), do: {:error, "Operação desconhecida."}
 end
