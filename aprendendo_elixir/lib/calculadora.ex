dcbf48c3ba65b8423713f01c491d0dcc09c1a9f3
morteerror404 <jfrota@stacksec.com.br>
2026-02-14 18:50:11 -0300

    Atualizando documentação

--- aprendendo_elixir/lib/calculadora.ex
+++ aprendendo_elixir/lib/calculadora.ex
@@ -3,6 +3,24 @@ defmodule Calculadora do
   Módulo de funções matemáticas básicas para o laboratório.
   """
 
-  # Aqui o Elixir escolhe a função baseada no átomo que passamos (:soma, :sub, etc)
+  def soma(a, b) do
+    a + b
+  end
+
+  def sub(a, b ) do
+    a - b
+  end
+
+  def mult(a, b) do
+    a * b
+  end
+  
+  def div(a, b) do
+    if b != 0 do
+      a / b
+    else
+      "Divisão por zero não é permitida."
+    end
+  end
 
 end
